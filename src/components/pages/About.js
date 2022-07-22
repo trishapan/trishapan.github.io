3a96166fb6e371669711cb69e27455c8d71e36ff
trisha <29530389+trisha@users.noreply.github.com>
2022-07-22 14:49:24 -0400

    Update About

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,7 +16,7 @@ function About(props) {
             <p>I spent 3 years with <a target="_blank" href="https://www.accenture.com/us-en">Accenture</a> doing technology consulting while based in San Francisco.</p>
             <p>I pivoted my career after doing a coding bootcamp with <a target="_blank" href="https://generalassemb.ly/education/software-engineering-immersive-remote">General Assembly</a> in March 2021.</p>
             <p>After working at 2 startups, now I'm seeking my next junior software engineering opportunity.</p>
-            <p>My dream is to one day become a CTO, but I'm not in a hurry. In my spare time, you can find me 
+            <p>My dream is to one day become a CTO or other type of executive, but I'm not in a hurry. In my spare time, you can find me 
                 painting or hosting paint n' sips, skiing blues, backpacking with a 700 lumen flashlight (because I'm scared of the dark),
                 doing improv with no shame (e.g. yodeling),
                 playing Genshin Impact on my phone,
